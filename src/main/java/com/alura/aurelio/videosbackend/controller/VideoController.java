19055a54cdb55d1660ee388168267ed6451c390f
MagnumWave <aureliomagno@terra.com.br>
2023-05-15 19:18:22 -0300

    adicionando status da requisição nos endpoints.

--- src/main/java/com/alura/aurelio/videosbackend/controller/VideoController.java
+++ src/main/java/com/alura/aurelio/videosbackend/controller/VideoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alura.aurelio.videosbackend.domain.Video;
@@ -37,16 +39,19 @@ public class VideoController {
 	}
 	
 	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public void post(@Validated(VideoInputDTO.class) @RequestBody VideoInputDTO videoDTO) throws CustomException {
 		service.criar(videoDTO.toVideo());
 	}
 	
 	@PutMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void put(@Validated(VideoInputDTO.class) @RequestBody VideoInputDTO videoDTO, @PathVariable Long id) throws CustomException {
 		service.atualizar(videoDTO.toVideo(), id);
 	}
 	
 	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
 	public void delete(@PathVariable Long id) throws CustomException {
 		service.remover(id);
 	}
